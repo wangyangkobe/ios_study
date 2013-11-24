d2ba106a8f98cc14bf37f376d98c32d327cc8be3
wangyang <250801860@qq.com>
2013-11-24 11:12:43 +0800

    Add 类别 for UILabel.

---
+++ MicroSpeaker/MicroSpeaker/UILabel+Extensions.h
@@ -0,0 +1,15 @@
+//
+//  UILabel+Extensions.h
+//
+//
+//  Created by wy on 13-11-24.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UILabel (Extensions)
+
+- (void)sizeToFitFixedWidth:(CGFloat)fixedWidth lines:(int)lineNumbers;
+
+@end
