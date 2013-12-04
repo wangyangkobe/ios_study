ac4b17b4164817742fb50d9c636bdb3fdca978a6
wangyang <250801860@qq.com>
2013-12-04 09:09:55 +0800

    增加宏文件。

---
+++ MicroSpeaker/MicroSpeaker/MacroDefination.h
@@ -0,0 +1,20 @@
+
+
+#ifndef MacroDefinition_h
+#define MacroDefinition_h
+
+
+#define SET_PROXY 0
+//-------------------获取设备大小-------------------------
+//NavBar高度
+#define NavigationBar_HEIGHT 44
+
+//ToolBar 高度
+#define TOOLBAR_HEIGHT 40
+//获取屏幕 宽度、高度
+#define SCREEN_WIDTH  ([UIScreen mainScreen].bounds.size.width)
+#define SCREEN_HEIGHT ([UIScreen mainScreen].bounds.size.height)
+
+//-------------------获取设备大小-------------------------
+
+#endif
