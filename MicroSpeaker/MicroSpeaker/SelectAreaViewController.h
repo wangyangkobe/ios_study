109669b1a538789407a5b91f95b053df44ab6d97
wangyang <250801860@qq.com>
2013-12-19 22:11:32 +0800

    实现选择社区以及2个view之间传递数据的功能。

---
+++ MicroSpeaker/MicroSpeaker/SelectAreaViewController.h
@@ -0,0 +1,25 @@
+//
+//  SelectAreaViewController.h
+//  MicroSpeaker
+//
+//  Created by wy on 13-12-19.
+//  Copyright (c) 2013年 yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class SelectAreaViewController;
+
+@protocol SelectAreaViewControllerDelegate <NSObject>
+
+-(void)selectAreaViewController:(SelectAreaViewController*)viewController didFinishSelectAreaId:(int)Id areaName:(NSString*)Name;
+
+@end
+
+@interface SelectAreaViewController : UITableViewController<UITableViewDataSource, UITableViewDelegate>
+{
+    NSArray* areasArray;
+}
+@property(nonatomic, assign) int areaId;
+@property(nonatomic, weak) id<SelectAreaViewControllerDelegate> delegate;
+@end
---
+++ MicroSpeaker/MicroSpeaker/SelectAreaViewController.m
@@ -0,0 +1,82 @@
+//
+//  SelectAreaViewController.m
+//  MicroSpeaker
+//
+//  Created by wy on 13-12-19.
+//  Copyright (c) 2013年 yang. All rights reserved.
+//
+
+#import "SelectAreaViewController.h"
+#import "NetWorkConnection.h"
+@interface SelectAreaViewController ()
+
+@end
+
+@implementation SelectAreaViewController
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    self.title = @"请选择推送社区";
+    dispatch_async(dispatch_get_global_queue(0, 0), ^{
+        areasArray = [NSArray arrayWithArray:[[NetWorkConnection sharedInstance] getArea]];
+        
+        dispatch_sync(dispatch_get_main_queue(), ^{
+            [self.tableView reloadData];
+        });
+    });
+    
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [areasArray count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    int row = [indexPath row];
+    static NSString *CellIdentifier = @"AreaTypeCell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+    }
+    AreaModel* area = [areasArray objectAtIndex:row];
+    cell.textLabel.text = [NSString stringWithFormat:@"%@,%@", area.AreaName, area.City];
+    if (self.areaId == area.AreaID) {
+        cell.imageView.image = [UIImage imageNamed:@"checked"];
+    }
+    else{
+        cell.imageView.image = [UIImage imageNamed:@"unchecked"];
+    }
+    return cell;
+}
+
+#pragma mark - Table view delegate
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    int row = [indexPath row];
+    AreaModel* area = [areasArray objectAtIndex:row];
+    self.areaId = area.AreaID;
+    [self.delegate selectAreaViewController:self didFinishSelectAreaId:self.areaId
+                                   areaName:[NSString stringWithFormat:@"%@,%@", area.AreaName, area.City]];
+    [self.tableView reloadData];
+}
+@end
