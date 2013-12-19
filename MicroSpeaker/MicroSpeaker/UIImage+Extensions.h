5c8a10fbb50098238c4a488cdc43d1cd28a81558
wangyang <250801860@qq.com>
2013-12-19 22:13:50 +0800

    实现图片的缩放功能。

---
+++ MicroSpeaker/MicroSpeaker/UIImage+Extensions.h
@@ -0,0 +1,13 @@
+//
+//  UIImage+Extensions.h
+//  
+//
+//  Created by wy on 13-12-18.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (Extensions)
+- (UIImage*)imageByScalingAndCroppingForSize:(CGSize)targetSize;
+@end
