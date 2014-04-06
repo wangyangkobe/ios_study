13093941d8fe8f57288a7aedc677bb66398104c5
wangyang <250801860@qq.com>
2014-04-06 21:15:04 +0800

    讲一些property增加Optional。

--- MicroSpeaker/MicroSpeaker/ClassModule/UserInfoModel.h
+++ MicroSpeaker/MicroSpeaker/ClassModule/UserInfoModel.h
@@ -13,17 +13,17 @@
 
 @property (nonatomic, copy)   AreaModel* Area;
 @property (nonatomic, copy)   NSString<Optional>* CreateAt;
-@property (nonatomic, copy)   NSString* Province;
-@property (nonatomic, copy)   NSString* City;
+@property (nonatomic, copy)   NSString<Optional>* Province;
+@property (nonatomic, copy)   NSString<Optional>* City;
 @property (nonatomic, copy)   NSString<Optional>* ModifiedAt;
 @property (nonatomic, assign) int Gender;
 @property (nonatomic, copy)   NSString<Optional>* QQ;
 @property (nonatomic, copy)   NSString<Optional>* WeiboID;
 @property (nonatomic, assign) int UserID;
-@property (nonatomic, copy)   NSString* HeadPic;
+@property (nonatomic, copy)   NSString<Optional>* HeadPic;
 @property (nonatomic, copy)   NSString<Optional>* HeadPicOriginal;
-@property (nonatomic, copy)   NSString* Country;
-@property (nonatomic, copy)   NSString* Description;
+@property (nonatomic, copy)   NSString<Optional>* Country;
+@property (nonatomic, copy)   NSString<Optional>* Description;
 @property (nonatomic, copy)   NSString* UserName;
 
 @end
