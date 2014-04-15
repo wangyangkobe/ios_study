245aa732d7589a534d262fb8d06adf7823fad053
wangyang <250801860@qq.com>
2014-04-15 21:32:04 +0800

    实现消息中心中 评论列表的显示。

--- MicroSpeaker/MicroSpeaker/ViewController/MessageViewController.m
+++ MicroSpeaker/MicroSpeaker/ViewController/MessageViewController.m
@@ -9,6 +9,9 @@
 #import "MessageViewController.h"
 #import "DAPagesContainer.h"
 #import "PrivateMessageViewController.h"
+#import "UILabel+Extensions.h"
+#import "NSString+Extensions.h"
+#import "SpeakerDetailViewController.h"
 
 #define kPrivateMessageVCTag 9000
 #define kCommentMessageVCTag 9001
@@ -50,6 +53,8 @@
         [[UIApplication sharedApplication].keyWindow setRootViewController:loginVC];
         return;
     }
+    [self configureScrollView];
+    _segmentControl.segmentedControlStyle = 7;
 }
 - (void)viewDidLoad
 {
@@ -63,18 +68,20 @@
     commentMessageTBV.dataSource = self;
     commentMessageTBV.delegate = self;
     commentMessageTBV.tag = kCommentMessageVCTag;
+    commentMessageTBV.contentInset = UIEdgeInsetsMake(0, 0, 90, 0); //tabbar height 49
     [_scrollView addSubview:commentMessageTBV];
     
     privateMessgaeTBV = [[UITableView alloc] init];
     privateMessgaeTBV.frame = CGRectMake(320, 0, 320, _scrollView.frame.size.height);
     privateMessgaeTBV.dataSource = self;
     privateMessgaeTBV.delegate = self;
+    privateMessgaeTBV.contentInset = UIEdgeInsetsMake(0, 0, 90, 0); //tabbar height 49
     privateMessgaeTBV.tag = kPrivateMessageVCTag;
     [_scrollView addSubview:privateMessgaeTBV];
-
+    
     dispatch_async(dispatch_get_global_queue(0, 0), ^{
         letterContacts  = [[[NetWorkConnection sharedInstance] getLetterContacts] mutableCopy];
-        commentContacts = [[[NetWorkConnection sharedInstance] getCommentToMe:-1 maxID:66666 num:3 page:1] mutableCopy];
+        commentContacts = [[[NetWorkConnection sharedInstance] getCommentToMe:-1 maxID:66666 num:10 page:1] mutableCopy];
         dispatch_async(dispatch_get_main_queue(), ^{
             if (0 == _segmentControl.selectedSegmentIndex)
                 [commentMessageTBV reloadData];
@@ -90,9 +97,8 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (void)initScrollView {
-    
-    //设置 tableScrollView
+- (void)configureScrollView
+{
     // a page is the width of the scroll view
     _scrollView.pagingEnabled = YES;
     _scrollView.clipsToBounds = NO;
@@ -110,7 +116,12 @@
     pageControl.currentPage = 0;
     pageControl.backgroundColor = [UIColor whiteColor];
 }
-
+- (CGFloat)calculateTextHeight:(NSString*)str
+{
+    return [str sizeWithFont:[UIFont fontWithName:@"Helvetica" size:16]
+           constrainedToSize:CGSizeMake(250, 100)
+               lineBreakMode:NSLineBreakByWordWrapping].height;
+}
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
     if(1 == _segmentControl.selectedSegmentIndex)
@@ -123,11 +134,16 @@
     if (_segmentControl.selectedSegmentIndex == 1)
         return 60;
     else
-        return 50;
+    {
+        CommentModel* comment = [commentContacts objectAtIndex:indexPath.row];
+        NSString* str = [NSString stringWithFormat:@"回复我的:%@", comment.Message.Text];
+        int height1 = [self calculateTextHeight:str];
+        int height2 = [self calculateTextHeight:comment.Text];
+        return height1 + height2 + 50;
+    }
 }
 - (UITableViewCell*)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    NSLog(@"%s", __FUNCTION__);
     if (_segmentControl.selectedSegmentIndex == 1)
     {
         static NSString* CellIdentifier = @"PrivateMessageCell";
@@ -140,55 +156,110 @@
             headPic.layer.cornerRadius = 5.0;
             [headPic setTag:8000];
             [cell.contentView addSubview:headPic];
-
+            
             UILabel* userName = [[UILabel alloc] initWithFrame:CGRectMake(65, 0, 200, 30)];
             [userName setTag:8001];
             [userName setTextColor:[UIColor grayColor]];
             [cell.contentView addSubview:userName];
-
+            
             UILabel* textLabel = [[UILabel alloc] initWithFrame:CGRectMake(65, 30, 200, 15)];
             [textLabel setTag:8002];
             [textLabel setFont:[UIFont systemFontOfSize:14]];
             [textLabel setTextColor:[UIColor grayColor]];
             [cell.contentView addSubview:textLabel];
-
-
+            
+            
             UILabel* timeLabel = [[UILabel alloc] initWithFrame:CGRectMake(65, 45, 200, 15)];
             [timeLabel setTag:8003];
             [timeLabel setFont:[UIFont systemFontOfSize:14]];
             [timeLabel setTextColor:[UIColor grayColor]];
             [cell.contentView addSubview:timeLabel];
-
+            
             cell.accessoryType = UITableViewCellAccessoryDetailDisclosureButton;
             cell.selectionStyle = UITableViewCellSelectionStyleNone;
         }
-
+        
         LetterModel* element = [letterContacts objectAtIndex:indexPath.row];
         UIImageView* headPic = (UIImageView*)[cell.contentView viewWithTag:8000];
         [headPic setImageWithURL:[NSURL URLWithString:element.User.HeadPic]
-            placeholderImage:[UIImage imageNamed:@"placeholder.png"]];
-
-
+                placeholderImage:[UIImage imageNamed:@"placeholder.png"]];
+        
+        
         UILabel* userName = (UILabel*)[cell.contentView viewWithTag:8001];
         [userName setText:element.User.UserName];
-
+        
         UILabel* text = (UILabel*)[cell.contentView viewWithTag:8002];
         [text setText:element.Letter.Text];
-
+        
         UILabel* time = (UILabel*)[cell.contentView viewWithTag:8003];
         [time setText:element.Letter.CreateAt];
         return cell;
     }
     else
     {
+        CommentModel* comment = [commentContacts objectAtIndex:indexPath.row];
+        NSString* messageText = [NSString stringWithFormat:@"回复我的:%@", comment.Message.Text];
+        float height1 = [self calculateTextHeight:messageText];
+        float height2 = [self calculateTextHeight:comment.Text];
+        
         static NSString* CellIdentifier = @"CommentMessageCell";
         UITableViewCell* cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
         if (cell == nil)
         {
+            for (UIView* view in [cell.contentView subviews])
+            {
+                if ([view isKindOfClass:[UILabel class]]) {
+                    [view removeFromSuperview];
+                }
+            }
             cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+            UIImageView* headPic = [[UIImageView alloc] initWithFrame:CGRectMake(5, 5, 50, 50)];
+            [headPic setTag:8005];
+            [cell.contentView addSubview:headPic];
+            
+            UILabel* nameLabel = [[UILabel alloc] initWithFrame:CGRectMake(60, 5, 200, 20)];
+            [nameLabel setTag:8006];
+            [nameLabel setFont:[UIFont systemFontOfSize:14]];
+            [nameLabel setTextColor:[UIColor grayColor]];
+            [cell.contentView addSubview:nameLabel];
+            
+            UILabel* textLable1 = [[UILabel alloc] initWithFrame:CGRectZero];
+            [textLable1 setTag:8007];
+            [textLable1 setFont:[UIFont systemFontOfSize:15]];
+            [cell.contentView addSubview:textLable1];
+            
+            UILabel* textLable2 = [[UILabel alloc] initWithFrame:CGRectZero];
+            [textLable2 setTag:8008];
+            [textLable2 setFont:[UIFont systemFontOfSize:15]];
+            [cell.contentView addSubview:textLable2];
+            
+            UILabel* timeLabel = [[UILabel alloc] initWithFrame:CGRectZero];
+            [timeLabel setTag:8009];
+            [timeLabel setFont:[UIFont systemFontOfSize:14]];
+            [timeLabel setTextColor:[UIColor grayColor]];
+            [cell.contentView addSubview:timeLabel];
         }
-        CommentModel* comment = [commentContacts objectAtIndex:indexPath.row];
-        cell.textLabel.text = comment.Text;
+        
+        UIImageView* headPic = (UIImageView*)[cell.contentView viewWithTag:8005];
+        [headPic setImageWithURL:[NSURL URLWithString:comment.UserBasic.HeadPic]
+                placeholderImage:[UIImage imageNamed:@"placeholder.png"]];
+        
+        UILabel* nameLabel = (UILabel*)[cell.contentView viewWithTag:8006];
+        [nameLabel setText:comment.UserBasic.UserName];
+        
+        UILabel* textLabel1 = (UILabel*)[cell.contentView viewWithTag:8007];
+        [textLabel1 setFrame:CGRectMake(60, 25, 250, height1)];
+        textLabel1.text = messageText;
+        [textLabel1 sizeToFitFixedWidth:250 lines:3];
+        
+        UILabel* textLabel2 = (UILabel*)[cell.contentView viewWithTag:8008];
+        [textLabel2 setFrame:CGRectMake(60, 25+height1, 250, height2)];
+        textLabel2.text = comment.Text;
+        [textLabel2 sizeToFitFixedWidth:250 lines:3];
+        
+        UILabel* timeLabel = (UILabel*)[cell.contentView viewWithTag:8009];
+        [timeLabel setFrame:CGRectMake(60, 25+height1+height2, 200, 20)];
+        [timeLabel setText:comment.CreateAt];
         return cell;
     }
 }
@@ -197,7 +268,7 @@
 {
     [tableView deselectRowAtIndexPath:indexPath animated:NO];
     UIStoryboard *mainStoryboard = [UIStoryboard storyboardWithName:@"MainStoryboard" bundle: nil];
-
+    
     if (_segmentControl.selectedSegmentIndex == 1)
     {
         PrivateMessageViewController* privateMessageVC = [mainStoryboard instantiateViewControllerWithIdentifier:@"PrivateMessageViewController"];
@@ -207,7 +278,13 @@
         [privateMessageVC setHidesBottomBarWhenPushed:YES];
         [self.navigationController pushViewController:privateMessageVC animated:YES];
     }
-
+    else
+    {
+        SpeakerDetailViewController* speakerDetailViewController = [[SpeakerDetailViewController alloc] init];
+        CommentModel* selectedComment= (CommentModel*)[commentContacts objectAtIndex:indexPath.row];
+        speakerDetailViewController.message = selectedComment.Message;
+        [self.navigationController pushViewController:speakerDetailViewController animated:YES];
+    }
 }
 - (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView
 {
@@ -217,6 +294,9 @@
     pageControl.currentPage = page;
     currentPage = page;
     pageControlUsed = NO;
+    
+    _segmentControl.selectedSegmentIndex = page;
+    [self changeTableView:nil];
 }
 - (IBAction)changeTableView:(id)sender
 {
